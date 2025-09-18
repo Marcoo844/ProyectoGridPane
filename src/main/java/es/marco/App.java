b830164df5258a64cdc1c478aefb9500e946fa22
Marcoo844 <maarcoo246@gmail.com>
2025-09-18 12:18:13 +0200

    Actualización con manifest y loggers

--- src/main/java/es/marco/App.java
+++ src/main/java/es/marco/App.java
@@ -5,14 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class App extends Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         Application.launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        logger.info("Lanzando la aplicacion");
+        logger.debug("Depuracion mostrada");
         Parent root = FXMLLoader.load(getClass().getResource("estilo.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module es.marco {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
+    requires org.slf4j;
 
     opens es.marco to javafx.fxml;
     exports es.marco;
